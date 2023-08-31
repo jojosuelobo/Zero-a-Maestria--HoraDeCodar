4b1bbcc41c44040150135ea2bafc086b15d6093f
jojosuelobo <josuelobo2000@gmail.com>
2023-08-31 16:50:44 -0300

    [129]

--- secao10-miniblog/src/pages/Register/Register.jsx
+++ secao10-miniblog/src/pages/Register/Register.jsx
@@ -1,51 +1,82 @@
+/* eslint-disable no-unused-vars */
 import styles from './Register.module.css';
 
 import { useState, useEffect } from 'react';
 
 export default function Register() {
-  return (
-    <div>
-        <h1>Cadastre-se para postar</h1>
-        <p>Crie seu usuário e compartilhe suas histórias</p>
-        <form>
-            <label>
-                <span>Nome:</span>
-                <input 
-                    type="text" 
-                    name='displayName' 
-                    required 
-                    placeholder='Nome de usuário' 
-                />
-            </label>
-            <label>
-                <span>E-mail:</span>
-                <input 
-                    type="email" 
-                    name='email' 
-                    required 
-                    placeholder='E-mail de usuário' 
-                />
-            </label>
-            <label>
-                <span>Senha:</span>
-                <input 
-                    type="password"  
-                    name='password' 
-                    required 
-                    placeholder='Insira sua senha' 
-                />
-            </label>
-            <label>
-                <span>Confirmar Senha:</span>
-                <input 
-                    type="password"  
-                    name='confirmPassword' 
-                    required 
-                    placeholder='Confirme sua senha' 
-                />
-            </label>
-            <button className='btn'>Cadastrar</button>
-        </form>
-    </div>
-  )
+    const [displayName, setDisplayName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setError("")
+        const user = {
+            displayName,
+            email,
+            password
+        }
+        if(password !== confirmPassword) {
+            setError("As senhas devem ser iguais")
+            return
+        }
+        console.log(user)
+    }
+
+    return (
+        <div className={styles.register}>
+            <h1>Cadastre-se para postar</h1>
+            <p>Crie seu usuário e compartilhe suas histórias</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Nome:</span>
+                    <input
+                        type="text"
+                        name='displayName'
+                        required
+                        placeholder='Nome de usuário'
+                        value={displayName}
+                        onChange={(e) => setDisplayName(e.target.value)}
+                    />
+                </label>
+                <label>
+                    <span>E-mail:</span>
+                    <input
+                        type="email"
+                        name='email'
+                        required
+                        placeholder='E-mail de usuário'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <label>
+                    <span>Senha:</span>
+                    <input
+                        type="password"
+                        name='password'
+                        required
+                        placeholder='Insira sua senha'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <label>
+                    <span>Confirmar Senha:</span>
+                    <input
+                        type="password"
+                        name='confirmPassword'
+                        required
+                        placeholder='Confirme sua senha'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                </label>
+                <button className='btn'>Cadastrar</button>
+                {error && <p className='error'>{error}</p>}
+            </form>
+        </div>
+    )
 }

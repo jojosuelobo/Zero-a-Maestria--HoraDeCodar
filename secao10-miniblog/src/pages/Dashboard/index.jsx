142046a8487b8b2e509a9e8978be7e84e1e58115
jojosuelobo <josuelobo2000@gmail.com>
2023-12-08 14:06:18 -0300

    renomeação e ajustes

--- secao10-miniblog/src/pages/Dashboard/index.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './Dashboard.module.css'
-
-export default function Dashboard() {
-  return (
-    <h1>Dashboard</h1>
-  )
-}
--- secao10-miniblog/src/pages/Home/Home.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-import styles from './Home.module.css'
-
-export default function Home() {
-  return (
-    <h1>Home</h1>
-  )
-}

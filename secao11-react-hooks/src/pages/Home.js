a7664b946b813bd1b2f1602b601645464c0becf8
josue <josue@octuspay.com>
2023-12-11 16:06:11 -0300

    useEffect

--- secao11-react-hooks/src/pages/Home.js
+++ secao11-react-hooks/src/pages/Home.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 // Custom Hooks
 import HookUseState from '../components/HookUseState'
 import HookUseReducer from '../components/HookUseReducer'
+import HookUseEffect from '../components/HookUseEffect'
 
 export default function Home() {
+
   return (
     <div>
       <hr />
       <HookUseState />
       <HookUseReducer />
+      <HookUseEffect/>
     </div>
   )
 }

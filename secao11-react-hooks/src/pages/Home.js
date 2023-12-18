d84562d95c3ef3b1fdd32918506d0e0677911606
josue <josue@octuspay.com>
2023-12-18 12:07:59 -0300

    useRef

--- secao11-react-hooks/src/pages/Home.js
+++ secao11-react-hooks/src/pages/Home.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import HookUseState from '../components/HookUseState'
 import HookUseReducer from '../components/HookUseReducer'
 import HookUseEffect from '../components/HookUseEffect'
+import HookUseRef from '../components/HookUseRef'
 
 import { useContext } from 'react'
 import { SomeContext } from '../components/HookUseContext'
@@ -20,6 +21,7 @@ export default function Home() {
       <h2>useContext</h2>
       <p>Valor do context: {contextValue}</p>
       <hr />
+      <HookUseRef/>
     </div>
   )
 }
